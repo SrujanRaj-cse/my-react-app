087712116696ad442873ed5cfd8069d4070dac7a
SrujanRaj-cse <reddycharlasrujanraj@gmail.com>
2025-07-29 15:54:35 +0530

    Build app and update dist folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import luffy from './assets/img1.jpeg';
+import pay_me from './assets/scanner_hemath.jpg';
 
 function App() {
   return (
     <div>
-      <h1>Hello from Srujan </h1>
+      <img src={luffy} alt="My image" />
+      <h1>PAY ME!! </h1>
+      <img src={pay_me} alt="My image1" />
     </div>
   );
 }
